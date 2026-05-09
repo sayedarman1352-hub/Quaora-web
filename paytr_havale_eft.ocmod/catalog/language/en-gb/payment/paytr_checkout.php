be9ec67647c1bd4d9aa3766d6a95bbc130d63183
Arman <sayedarman1352@gmail.com>
2026-05-09 23:07:42 +0300

    dgaagdsgs

--- paytr_havale_eft.ocmod/catalog/language/en-gb/payment/paytr_checkout.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_['text_title'] = 'Pay by Credit/Debit Card - PayTR';
-$_['button_confirm'] = 'Pay with PayTR';
-$_['text_loading'] = 'Preparing payment page...';
-$_['error_paytr_checkout_curl'] = 'cURL is not enabled on the server.';
-$_['error_paytr_iframe_failed'] = 'PayTR token failed: ';
-$_['error_paytr_checkout_transaction_save'] = 'PayTR transaction could not be saved.';
